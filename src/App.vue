464c6d6de0b37a87cb6ff8f76cd957667e2c73fa
lsbin <lishubin@lisdeMacBook-Pro.local>
2018-11-15 17:10:22 +0800

    style的修改

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   }
 }
 </script>
-<style lang="stylus">
+<style lang="stylus" type="text/stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
